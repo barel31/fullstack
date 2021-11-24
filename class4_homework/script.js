b63cfaf673dfa2a2aac3f673e86b1119b9dd37af
barel31 <barel123133@gmail.com>
2021-11-24 20:22:51 +0200

    Update script.js
    with new pratices

--- class4_homework/script.js
+++ class4_homework/script.js
@@ -8,7 +8,6 @@ console.log(p1Var1, p1Var2, p1Var3);
 console.log("Multi: " + p1Var1 * p1Var2 * p1Var3);
 
 
-// practice 2 || Swaping between two variables
 console.log("-------------------------------------------");
 console.log("practice 2 || Swaping between two variables");
 let p2Var1 = "first var";
@@ -22,7 +21,6 @@ console.log("p2Var1: " + p2Var1);
 console.log("p2Var2: " + p2Var2);
 
 
-// practice 3 || Replace space with asterisk
 console.log("-------------------------------------------");
 console.log("practice 3 || Replace space with asterisk");
 let p3Var1 = "Barel Shraga";
@@ -33,7 +31,6 @@ let replaced = p3Var1.slice(0, space) + '*' + p3Var1.slice(space+1);
 console.log(replaced);
 
 
-// practice 4 || Exponent
 console.log("-------------------------------------------");
 console.log("practice 4 || Exponent");
 let p4Var1 = 5;
@@ -42,7 +39,6 @@ console.log(p4Var1, p4Var2);
 console.log(p4Var1**p4Var2);
 
 
-// practice 5 || Insert before middle after
 console.log("-------------------------------------------");
 console.log("practice 5 || Insert before middle after");
 let p5Var1 = '_';
@@ -55,7 +51,6 @@ p5Var2 = p5Var1 + p5Var2 + p5Var1;
 console.log(p5Var2);
 
 
-// practice 6 || Sum of first digit
 console.log("-------------------------------------------");
 console.log("practice 6 || Sum of first digit");
 let p6Var1 = 12;
@@ -68,7 +63,6 @@ console.log("a. Sum: " + (p6Var1+p6Var2+p6Var3+p6Var4+p6Var5));
 console.log("b. First digit sum: " + (p6Var1%10+p6Var2%10+p6Var3%10+p6Var4%10+p6Var5%10));
 
 
-// practice 7 || Length of string plus string with space between
 console.log("-------------------------------------------");
 console.log("practice 7 || String plus string with space between");
 let p7Var1 = 'String';
@@ -80,7 +74,6 @@ console.log(p7Var3);
 console.log(p7Var3.length);
 
 
-// practice 8 || Length of first+last name
 console.log("-------------------------------------------");
 console.log("practice 8 || Length of first+last name");
 let p8Var1 = 'Barel Shraga';
@@ -89,25 +82,70 @@ console.log(p8Var1.slice(0, p8Var1.indexOf(' ')).length);
 console.log(p8Var1.slice(p8Var1.indexOf(' ')+1).length);
 
 
-// practice 9 || ?!?!
 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 console.log("practice 9 || ?!?!");
 
 
-// practice 10 || ?!?!
 console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 console.log("practice 10 || ?!?!");
 
 
-// practice 11 || Swap two numbers without variable
 console.log("-------------------------------------------");
 console.log("practice 11 || Swap two numbers without variable");
 let first = 1;
 let second = 5;
 console.log(first);
 console.log(second);
-first = first + second; // 6
-second = first - second; // 1
-first = first - second; // 5
+first = first + second;
+second = first - second;
+first = first - second;
 console.log(first);
 console.log(second);
+
+
+
+
+console.log("-------------------------------------------");
+console.log("------------- More practice 1 -------------");
+console.log("-------------------------------------------");
+console.log("practice extra 1 || Find index of chars b, e in abcdegh");
+let str = 'abcdefgh';
+console.log('Index of "a": ' + str.indexOf('b'));
+console.log('Index of "b": ' + str.indexOf('e'));
+
+
+console.log("-------------------------------------------");
+console.log("practice extra 2 || Slice string to new string");
+str = 'I really like fruits and vegetables';
+let str2 = str.slice(14, 19);
+console.log(str2);
+
+
+console.log("-------------------------------------------");
+console.log("practice extra 3 || Work with 3 strins");
+let firstName = "Barel";
+let lastName = "Shraga";
+let age = "24";
+console.log("My name is", firstName, lastName, "my age is", age);
+
+
+console.log("-------------------------------------------");
+console.log("practice extra 4 || Alert 2 string");
+let firstStr = "First string here";
+let secondStr = "Second string here";
+// alert(firstStr);
+// alert(secondStr);
+
+console.log("-------------------------------------------");
+console.log("practice extra 5 || Alert 2 string");
+let lowerCaseStr = "case";
+console.log(lowerCaseStr);
+console.log(lowerCaseStr.toUpperCase());
+
+
+console.log("-------------------------------------------");
+console.log("practice extra 6 || Revert string at half from prompt");
+let input = prompt("Enter a string to revert: ", "defabc");
+let firstSlice = input.slice(0, input.length/2);
+let secondSlice = input.slice(input.length/2);
+console.log("Reversed:", secondSlice+firstSlice);
