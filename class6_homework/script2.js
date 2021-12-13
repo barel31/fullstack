2f9dad7c2bed068b62dbdbeca0e16a2b4aaa6b20
barel31 <barel123133@gmail.com>
2021-12-14 00:16:16 +0200

    Update script2.js

--- class6_homework/script2.js
+++ class6_homework/script2.js
@@ -1,79 +1,73 @@
-// function p1(num) {
-//     debugger
-//     for(let i = 0; i < num.slice(); i++) {
-//         for(let j = num.length; j < num.length/2; j--) {
-//             if(num.charAt(i) != num.charAt(j)) {
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
+function p1(num) {
+    for (let i = 0; i < num; i++) {
+        let last = num % 10;
+        let first = getFirstDigit(num);
 
-// console.log(p1(121));
+        if (last != first[1]) {
+            return false;
+        }
+        let remove = first[1];
+        for (let i = 0; i < first[0]; i++) {
+            remove *= 10;
+        }
+        num -= remove; // remove first digit
+        num = Math.floor(num / 10); // remove last digit
+    }
+
+    return true;
+}
+function getFirstDigit(num) {
+    let count = 0;
+    while (num >= 10) {
+        num = num / 10;
+        count++;
+    }
+    return [count, Math.floor(num)];
+}
+// console.log(p1(112211));
 // console.log(p1(849210));
 // console.log(p1(44566544));
-// console.log(p1('aabbs'));
+// console.log(p1(14566547));
 
-// BARMUDA meshulash
-// function p4(num) {
-//     let firstAndLast = '*', middle = '', space = '';
-
-//     for(let i = 2; i <= num; i++) {
-//         firstAndLast += '*';
-//         space += ' ';
-//         if(i) {
-//             middle += '\n'
-//         }
-//         middle += '*' + space.slice(1) + '*'
-//     }
-//     for(let i = 0; i < num-2; i++) {
-//     }
-
-//     if(num > 2) {
-//         console.log(firstAndLast+'\n'+middle+'\n'+firstAndLast);
-//     } else {
-//         console.log(firstAndLast+'\n'+middle);
-//     }
-// }
-
-function p4(num) {
-    let firstAndLast = "*",
-        middle = "",
-        space = "";
-
-    for (let i = 2; i <= num; i++) {
-        firstAndLast += "*";
-        space += " ";
+function p2(email) {
+    if (email.length < 5) {
+        return false;
     }
-    for (let i = 0; i < num - 2; i++) {
-        if (i) {
-            middle += "\n";
+
+    let shtrudelIndex = email.indexOf("@");
+    if (
+        shtrudelIndex == -1 ||
+        !is_contain_alpha(email.slice(0, shtrudelIndex)) ||
+        !is_contain_alpha(email.slice(shtrudelIndex + 1))
+    ) {
+        return false;
+    }
+
+    let ending = email.slice(email.indexOf(".") + 1);
+    if (ending.length < 2) {
+        return false;
+    }
+    for (let i = 0; i < ending.length; i++) {
+        if (!is_contain_alpha(ending.charAt(i))) {
+            return false;
         }
-        middle += "*" + space.slice(1) + "*";
     }
 
-    if (num > 2) {
-        console.log(
-            num + "\n" + firstAndLast + "\n" + middle + "\n" + firstAndLast
-        );
-    } else {
-        console.log(num + "\n" + firstAndLast);
-    }
+    return true;
 }
-
-// p4(1);
-// p4(2);
-// p4(3);
-// p4(4);
-// p4(5);
-// p4(6);
-// p4(7);
-// p4(8);
-// p4(9);
-// p4(10);
-// p4(20);
-// p4(30);
+function is_contain_alpha(str) {
+    // Atleast 1 char in the str
+    for (let i = 0; i < str.length; i++) {
+        if (
+            (str[i] >= "A" && str[i] <= "Z") ||
+            (str[i] >= "a" && str[i] <= "z")
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
+// console.log(p2("barel@1sdasd.cm"));
 
 function p3(date, num) {
     let day = "",
@@ -99,8 +93,7 @@ function p3(date, num) {
         }
     }
     console.log("day " + day + " | month " + month + " | year " + year); // parsing date
-    
-    // debugger
+
     // Convert date
     let days = (Number(day) + num) % 30;
     if (!days) days++;
@@ -122,4 +115,85 @@ function p3(date, num) {
 }
 
 // console.log(p3("30.2.2020", 51));
-console.log(p3("1.1.2010", 365));
+// console.log(p3("1.1.2010", 365));
+
+function p4(num) {
+    let firstAndLast = "*",
+        middle = "",
+        space = "";
+
+    for (let i = 2; i <= num; i++) {
+        firstAndLast += "*";
+        space += " ";
+    }
+    for (let i = 0; i < num - 2; i++) {
+        if (i) {
+            middle += "\n";
+        }
+        middle += "*" + space.slice(1) + "*";
+    }
+
+    if (num > 2) {
+        console.log(
+            num + "\n" + firstAndLast + "\n" + middle + "\n" + firstAndLast
+        );
+    } else {
+        console.log(num + "\n" + firstAndLast);
+    }
+}
+// p4(1);
+// p4(2);
+// p4(3);
+// p4(4);
+// p4(5);
+// p4(6);
+// p4(7);
+// p4(8);
+// p4(9);
+// p4(10);
+// p4(20);
+// p4(30);
+
+// BARMUDA meshulash
+// function p5(num) {
+//     let firstAndLast = '*', middle = '', space = '';
+
+//     for(let i = 1; i <= num; i++) {
+//         firstAndLast += '*';
+//         space += ' ';
+//         if(i) {
+//             middle += '\n'
+//         }
+//         middle += '*' + space.slice(1) + '*'
+//     }
+//     for(let i = 0; i < num-2; i++) {
+//     }
+
+//     if(num > 2) {
+//         console.log(middle+'\n'+firstAndLast);
+//     } else {
+//         console.log(firstAndLast+'\n'+middle);
+//     }
+// }
+
+function p5(num) {
+    let last = '\n', spaces = '', first='';
+    for(let i = 1; i < num/2; i++) {
+        first += ' ';
+    }
+    first += '*'
+
+    for(let i = 1; i <= num; i++) {
+
+        last += '*'
+    }
+    for(let i = 1; i <= num; i++) {
+        spaces += ' ';
+        for(let j = num/2; j > 0; j--) {
+            spaces += '*'
+        }
+        spaces += '*\n'
+    }
+    console.log(first+spaces+last);
+}
+p5(5);
