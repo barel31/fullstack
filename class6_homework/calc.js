7db9520eb3e0a097c7741efe96366ed49af53eb2
barel31 <barel123133@gmail.com>
2021-12-18 21:41:55 +0200

    Update calc.js

--- class6_homework/calc.js
+++ class6_homework/calc.js
@@ -1,31 +1,33 @@
-let output = '', action = '';
-let calc, ;
+let output = "",
+    action = "";
+let calc;
 
 function Btn(val) {
     output += val;
     console.log(val);
 
-    if (val == 'C') {
+    if (val == "C") {
         clear();
-    } else if (val != '=' && !Number(val)) {
+    } else if (val != "=" && !Number(val)) {
         action += val;
     }
 
-    Update(val == '=');
+    Update(val == "=");
 }
 
 function clear() {
-    output = '';
-    action = '';
+    output = "";
+    action = "";
 }
 
 // Update output
-function Update(equal = false) { // TODO - think of a way to make order of operations.
-    let outputElement = document.getElementById('calcInput');
+function Update(equal = false) {
+    // TODO - think of a way to make order of operations.
+    let outputElement = document.getElementById("calcInput");
     outputElement.value = output;
     // Button equal pressed
     if (equal) {
-        debugger
+        debugger;
         // const math = output.split(/[-+*/=]/);
         // find a way to skip regex from first char,
         // it's doesn't makes error tho.
@@ -34,27 +36,27 @@ function Update(equal = false) { // TODO - think of a way to make order of opera
         console.log(action);
         // Handling math
         let res = Number(math[0]);
-        for(let i = 0; i < action.length; i++) {
-            if (action[i] == '+') {
-                res += Number(math[i+1]);
-            } else if (action[i] == '-') {
-                res -= Number(math[i+1]);
-            } else if (action[i] == '*') {
-                res *= Number(math[i+1]);
+        for (let i = 0; i < action.length; i++) {
+            if (action[i] == "+") {
+                res += Number(math[i + 1]);
+            } else if (action[i] == "-") {
+                res -= Number(math[i + 1]);
+            } else if (action[i] == "*") {
+                res *= Number(math[i + 1]);
             } else {
-                res /= Number(math[i+1]);
+                res /= Number(math[i + 1]);
             }
         }
         output += res;
 
-        if (output.indexOf('NaN') == -1/* && math.length == 2*/) {
+        if (output.indexOf("NaN") == -1 /* && math.length == 2*/) {
             // Valid
             outputElement.value = output;
-            clear()
+            clear();
         } else {
             // Invalid
-            outputElement.value = 'ERROR';
-            clear()
+            outputElement.value = "ERROR";
+            clear();
         }
     }
 }
