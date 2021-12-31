443405c4705d3f3dd6f74ba32eddcde36ab67749
barel31 <barel123133@gmail.com>
2021-12-31 22:00:15 +0200

    reformat tab-space

--- dice/script.js
+++ dice/script.js
@@ -1,19 +1,19 @@
 function Refresh() {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let players = document.getElementsByClassName("img");
+    players[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    players[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-  let players = document.getElementsByClassName("img");
-  players[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-  players[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
-
-  let result = document.getElementById('h1');
-  if (randomNumber1 > randomNumber2) {
-      result.textContent = " 🚩Player 1 Wins!";
-  } else if (randomNumber1 < randomNumber2) {
-      result.textContent = "Player 2 Wins! 🚩";
-  } else {
-      result.textContent = "🎌Draw!🎌";
-  }
+    let result = document.getElementById("h1");
+    if (randomNumber1 > randomNumber2) {
+        result.textContent = " 🚩Player 1 Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        result.textContent = "Player 2 Wins! 🚩";
+    } else {
+        result.textContent = "🎌Draw!🎌";
+    }
 }
 Refresh();
+// TODO: Add history outpot
