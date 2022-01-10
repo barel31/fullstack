515e4f986ef1bf161b7a60c68c7860b9ccdd32e6
barel31 <barel123133@gmail.com>
2022-01-10 18:39:26 +0200

    Successfully printing soduku

--- suduko/game.js
+++ suduko/game.js
@@ -5,38 +5,36 @@ function initBoard() {
     let board = document.getElementById('board');
 
     let ele;
-    let groupCounter = 1;
+    let group = 1;
+    let group2 = 1;
     for(let i = 1; i <= 9; i++) {
-        let index = 1;
         for(let j = 1; j <= 9; j++) {
-            
 
             ele = document.createElement('input');
-            let groupClass = 'group' + index;
+            let groupClass = 'group' + group;
             ele.classList.add('input');
             ele.classList.add(groupClass);
             ele.setAttribute('name', 'input_' + i + '_' + j);
             ele.setAttribute('type', 'text');
             ele.setAttribute('maxlength', '1');
-            ele.setAttribute('oninput', "this.value=this.value.replace(/[^1-9]/g,'');"); // 1-9 only
-            if(i % 3 === 0 && j % 3 === 0) {
-                groupCounter++;
-                ele.value = 1;
-                
-            }
+            // ele.setAttribute('oninput', "this.value=this.value.replace(/[^1-9]/g,'');"); // 1-9 only
             board.appendChild(ele);
 
-            if(j % 9 === 0) {
-                // ele.value = 1;
-                br = document.createElement('br');
-                board.appendChild(br);
-            }
             if(j % 3 === 0) {
-                index++;
+                group++;
+            }
+            if(j % 9 === 0) {
+                let br = document.createElement('br')
+                board.appendChild(br)
+                group2 = group;
+                group -= 3;
+            }
+            if(j === 9 && i % 3 === 0) {
+                group++ 
             }
         }
         if(i % 3 === 0) {
-            index++;
+            group = group2;
         }
     }
 }
