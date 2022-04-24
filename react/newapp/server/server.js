33aa062c9cfa62691493389b0d39c9e652665d4b
Barel Shraga <barel123133@gmail.com>
2022-04-24 17:19:05 +0300

    some projects

--- react/newapp/server/server.js
+++ react/newapp/server/server.js
@@ -4,9 +4,9 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'));
+// });
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up!');
 });
