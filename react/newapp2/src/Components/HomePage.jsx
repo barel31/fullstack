650a8f5cf9c4926201e310e50feed1a842943a98
barel31 <barel123133@gmail.com>
2022-04-27 15:12:32 +0300

    fix error in navigation after validate name and password

--- react/newapp2/src/Components/HomePage.jsx
+++ react/newapp2/src/Components/HomePage.jsx
@@ -1,20 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Context from '../Context';
 
 export default function HomePage() {
     const navigate = useNavigate();
-    const { fullname, setFullname, setPassword, validMsgs, validFullname, validPassword } = useContext(Context);
+    const { fullname, setFullname, password, setPassword, validMsgs, validFullname, validPassword } = useContext(
+        Context
+    );
 
-    if (validFullname() && validPassword()) {
-        navigate(`/${fullname}`);
-    }
+    useEffect(() => {
+        if (validFullname() && validPassword()) {
+            navigate(`/${fullname}`);
+        }
+    }, [fullname, password]);
 
     return (
         <div className='HomePage'>
             {validFullname() && validPassword() ? null : (
                 <>
-                <h2>Register</h2>
+                    <h2>Register</h2>
                     {validMsgs(1)}
                     <input type='text' placeholder='Fullname' onChange={(e) => setFullname(e.target.value)} />
                     {validMsgs(2)}
