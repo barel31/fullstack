d92ff74b58aab137eda479c18c647b22381cde65
Barel Shraga <barel123133@gmail.com>
2022-06-14 18:29:34 +0300

    Markup projects links also archer links open in a new tab

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 class Data {
     length = 0;
 
-    constructor(name, text, link) {
+    constructor(name, text, link, mark = false) {
         this.name = name;
         this.text = text;
         this.link = link;
+        this.mark = mark;
 
         length++;
     }
 }
 
 const data = [
-    new Data('Trivia Game', 'Play!', 'https://evening-oasis-66395.herokuapp.com/play'),
+    new Data('Trivia Game', 'Play!', 'https://evening-oasis-66395.herokuapp.com/play', true),
     new Data('class-1 CV DEMO', 'index.html', 'class1_homework/index.html'),
-    new Data('class-2 signin/up templates', 'signin.html signup.html', 'class2_homework/signin.html class2_homework/signup.html'),
-    new Data('class-3 Bootstrap template', 'index.html', 'class3_homework/index.html'),
+    new Data(
+        'class-2 signin/up templates',
+        'signin.html signup.html',
+        'class2_homework/signin.html class2_homework/signup.html'
+    ),
+    new Data('class-3 Bootstrap template', 'index.html', 'class3_homework/index.html', true),
     new Data('class-4 Summary of compiler and interpreter', 'index.html', 'class4_homework/index.html'),
     new Data('class-5 - DEMO Exchanger', 'index.html', 'class5_homework/index.html'),
     new Data('class-6 - Simple Calculator', 'index.html', 'class6_homework/index.html'),
-    new Data('Dice Game', 'Play!', 'dice/dicee.html'),
-    new Data('Drum Kit', 'Play!', 'drum/index.html'),
-    new Data('Simon Say', 'Play!', 'simon/index.html'),
-    new Data('Sudoku', 'Play!', 'sudoku/index.html'),
+    new Data('Dice Game', 'Play!', 'dice/dicee.html', true),
+    new Data('Drum Kit', 'Play!', 'drum/index.html', true),
+    new Data('Simon Say', 'Play!', 'simon/index.html', true),
+    new Data('Sudoku', 'Play!', 'sudoku/index.html', true),
     new Data('Grade Graph Server Side', 'Heroku', 'https://evening-plains-58384.herokuapp.com/'),
-    new Data('class-10 Signin/Signup Server Side', 'Heroku', 'https://still-ravine-70553.herokuapp.com/'),
-    new Data('War Game', 'Play!', 'https://barel31.github.io/war-game/'),
-    new Data('Smart-House', 'Go!', 'https://barel31.github.io/smart-house/'),
+    new Data('class-10 Signin/Signup Server Side', 'Heroku', 'https://still-ravine-70553.herokuapp.com/', true),
+    new Data('War Game', 'Play!', 'https://barel31.github.io/war-game/', true),
+    new Data('Smart-House', 'Go!', 'https://barel31.github.io/smart-house/', true),
     new Data('Tenbis Wage Calculator', 'Go!', 'https://barel31.github.io/tenbis-calculator/'),
-    new Data('Best Movie', 'Go!', 'https://barel31.github.io/best-movie/'),
-    new Data('Travel Advisor', 'Go!', 'https://barel31.github.io/travel_advisor/'),
+    new Data('Best Movie', 'Go!', 'https://barel31.github.io/best-movie/', true),
+    new Data('Travel Advisor', 'Go!', 'https://barel31.github.io/travel_advisor/', true),
 ];
 
 const table = document.getElementById('table');
@@ -38,6 +43,7 @@ table.appendChild(tbody);
 
 for (let i = 0; i < data.length; i++) {
     const tr = document.createElement('tr');
+    if (data[i].mark) tr.style.backgroundColor = '#FEF9A7';
     tbody.appendChild(tr);
 
     const td = document.createElement('td');
@@ -56,8 +62,8 @@ for (let i = 0; i < data.length; i++) {
         const link = document.createElement('a');
         link.innerHTML = textArr[j];
         link.setAttribute('href', linkArr[j]);
+        link.setAttribute('target', '_blank');
         link.style.marginRight = '1vh';
         td2.appendChild(link);
     }
 }
-
