8b611735df49a985f6281e06de4ef389a0323dc6
Barel Shraga <barel123133@gmail.com>
2022-06-14 18:56:36 +0300

    Added link for portfolio

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ const data = [
     new Data('Tenbis Wage Calculator', 'Go!', 'https://barel31.github.io/tenbis-calculator/'),
     new Data('Best Movie', 'Go!', 'https://barel31.github.io/best-movie/', true),
     new Data('Travel Advisor', 'Go!', 'https://barel31.github.io/travel_advisor/', true),
+    new Data('Portfolio', 'Go!', 'https://barel31.github.io/barel_portolio/', true),
 ];
 
 const table = document.getElementById('table');
